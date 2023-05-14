1d56500802b93729c3de7ec5d9f4b579220e58de
rerecum <kacperkacperkacper132@gmail.com>
2023-05-14 14:26:36 +0200

    First condition, set progressbar to zero, find all of id's and change editText box value to int

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.progressbar
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPaseId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.sort).setOnClickListener {
+            findViewById<ProgressBar>(R.id.progressBar).progress = 0
+            findViewById<LinearLayout>(R.id.unsorted).removeAllViews()
+            findViewById<LinearLayout>(R.id.sorted).removeAllViews()
 
+            val ListLength = if (findViewById<EditText>(R.id.numbers).text.toString()!="")
+                findViewById<EditText>(R.id.numbers).text.toString().toInt()
+            else 10
+        }
     }
 }
