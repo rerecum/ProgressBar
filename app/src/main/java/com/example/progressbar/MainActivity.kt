f88a3b960f50fff751bd73949601f862c064cecb
rerecum <kacperkacperkacper132@gmail.com>
2023-05-14 14:52:18 +0200

    Changed progressbar to horizontal

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.progressbar
 
 import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.loader.content.AsyncTaskLoader
 import java.util.*
 import kotlin.streams.toList
 
@@ -24,24 +26,26 @@ class MainActivity : AppCompatActivity() {
             val ListLength = if (findViewById<EditText>(R.id.numbers).text.toString()!="")
                 findViewById<EditText>(R.id.numbers).text.toString().toInt()
             else 10
-
-            val currentTime = System.currentTimeMillis()
-            val ListToSort = Random().ints(ListLength.toLong()).toList().toMutableList()
-            for (element in ListToSort){
-                val textToAdd = TextView(this.baseContext)
-                textToAdd.text="$element"
-                runOnUiThread { findViewById<LinearLayout>(R.id.unsorted).addView(textToAdd) }
-            }
-            val listSorted:List<Int> = sortWithProgress(ListToSort, findViewById(R.id.progressBar))
-            runOnUiThread { findViewById<TextView>(R.id.time).text="Czas sortowania: ${"%.2f".format((System.currentTimeMillis()-currentTime).toDouble()/1000)} sekund" }
-            for (element in listSorted) {
-                val textToAdd = TextView(baseContext)
-                textToAdd.text = "$element"
-                runOnUiThread { findViewById<LinearLayout>(R.id.sorted).addView(textToAdd) }
-            }
+                val currentTime = System.currentTimeMillis()
+                val ListToSort = Random().ints(ListLength.toLong()).toList().toMutableList()
+                for (element in ListToSort) {
+                    val textToAdd = TextView(this.baseContext)
+                    textToAdd.text = "$element"
+                    runOnUiThread { findViewById<LinearLayout>(R.id.unsorted).addView(textToAdd) }
+                }
+                val listSorted: List<Int> =
+                    sortWithProgress(ListToSort, findViewById(R.id.progressBar))
+                runOnUiThread {
+                    findViewById<TextView>(R.id.time).text =
+                        "Czas sortowania: ${"%.2f".format((System.currentTimeMillis() - currentTime).toDouble() / 1000)} sekund"
+                }
+                for (element in listSorted) {
+                    val textToAdd = TextView(baseContext)
+                    textToAdd.text = "$element"
+                    runOnUiThread { findViewById<LinearLayout>(R.id.sorted).addView(textToAdd) }
+                }
         }
     }
-}
 private fun sortWithProgress(list: MutableList<Int>, progressBar: ProgressBar): List<Int> {
     val size = list.size
     for (i in 0 until size - 1) {
@@ -55,4 +59,5 @@ private fun sortWithProgress(list: MutableList<Int>, progressBar: ProgressBar): 
         progressBar.progress = ((i + 1).toDouble() / (size - 1) * 100).toInt()
     }
     return list
+    }
 }
