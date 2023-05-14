744d05ac67795dbdb7ee6af7f708bb5269a1c90b
rerecum <kacperkacperkacper132@gmail.com>
2023-05-14 14:48:21 +0200

    Everything is working apart from progress bar. First trap of my programme...

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlin.streams.toList
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
-    @SuppressLint("CutPaseId", "SetTextI18n")
+    @SuppressLint("CutPaseId", "SetTextI18n", "CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,4 +41,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
+private fun sortWithProgress(list: MutableList<Int>, progressBar: ProgressBar): List<Int> {
+    val size = list.size
+    for (i in 0 until size - 1) {
+        for (j in 0 until size - i - 1) {
+            if (list[j] > list[j + 1]) {
+                val temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+            }
+        }
+        progressBar.progress = ((i + 1).toDouble() / (size - 1) * 100).toInt()
+    }
+    return list
 }
